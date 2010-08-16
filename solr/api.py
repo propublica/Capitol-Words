71572bd06332ee3891ccb63f7581e2aabf8659c8
jessykate <jessy.cowansharp@gmail.com>
2010-08-16 14:28:25 -0400

    stubbing out some interface functions for the frontend

---
+++ solr/api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+''' A set of functions that give specific views into the solr documents,
+returning nicely formatted statistics.  '''
+
+# return dicts of dates and frequency counts, plus links back to the raw ascii
+# documents
+def phrase_by_person(phrase, name):
+    pass
+
+def phrase_by_state(phrase, state):
+    pass
+
+def phrase_by_party(phrase, party):
+    pass
+
+def phrase_by_date(phrase, date):
+    pass
+
+def most_frequent_by_person(date):
+    pass
+
+def most_frequent_on_day(person):
+    pass
+
+def most_frequent_over_daterange(start, end):
+    pass
+
+
+
+
