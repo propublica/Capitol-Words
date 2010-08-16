78a60402c9a7508ea8f29196ef90bc52c78723c9
jessykate <jessy.cowansharp@gmail.com>
2010-08-15 22:29:34 -0400

    congress.gov bioguide scraper

--- solr/lib.py
+++ solr/lib.py
@@ -5,8 +5,8 @@
 from settings import API_KEY
 
 from BeautifulSoup import BeautifulSoup
-import urllib2
-import urllib
+import urllib2, urllib, re
+
 try:
     import json
 except:
@@ -109,48 +109,40 @@ def sunlight_lookup(lastname, state=None):
                 legislator = result['result']['legislator'] 
                 return legislator
 
-def bioguide_lookup(lastname, state=None):
-    ''' to be used if the sunlight API did not have data about this legislator'''
+def bioguide_lookup(lastname, year, state=None):
+    ''' looks up full name, bioguide id, state, and party based on last name, year
+    and optionally, state. returns one or more results. lastname and year are
+    usually all that's required to return a unique result, but with common names if
+    multiple legislators have the same last name in the same congress, the state
+    argument is also needed to uniquely specify. with all three, there *should*
+    only be a single result.'''
 
-    # search the site
-    arguments = {'lastname': lastname}
-    if abbr(state):
+    arguments = {'lastname': lastname, 'congress': year}
+    if state and abbr(state):
         arguments['state'] = abbr(state).upper()
     url = "http://bioguide.congress.gov/biosearch/biosearch1.asp"
-    fp = urllib2.urlopen(url, urllib.urlencode(arguments))
-    print fp.read()
-
-
-def temp():
-
-    # scrape the results page
-    # get info about each of them
-    # store it locally for next time?
-
-    # scrape congress's bioguide site for years of service and official bio
-    html = urllib2.urlopen("http://bioguide.congress.gov/scripts/biodisplay.pl?index=%s" % bioguide_id).read()
-    soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
-    yrs_of_service = soup.findAll('table')[1].find('tr').findAll('td')[1].findAll('font')[2].next.next.next.strip()
-    bio_a = soup.findAll('table')[1].find('tr').findAll('td')[1].find('p').find('font').extract().renderContents()
-    bio_b = soup.findAll('table')[1].find('tr').findAll('td')[1].find('p').renderContents()
-    biography = bio_a.strip()+' '+bio_b.strip()
-
-    # other metadata - from sunlightlabs services
-    arguments = urllib.urlencode({'apikey': API_KEY,
-                                  'bioguide_id': bioguide_id,
-                                  'all_legislators': 1,
-                                  })
-    url = "http://services.sunlightlabs.com/api/legislators.get.json?"
-    api_call = url + arguments
-    print api_call
-    fp = urllib2.urlopen(api_call)
-    js = json.loads(fp.read())
-    meta = js['response']['legislator']
-
-    # append additional info and return
-    meta['photo_url'] = photo_url
-    meta['yrs_of_service'] = yrs_of_service
-    meta['biography'] = biography
-    return meta
+    html_lines = urllib2.urlopen(url, urllib.urlencode(arguments)).readlines()
+    re_result_firstline = r'<tr><td><a href="http://bioguide\.congress\.gov/scripts/biodisplay\.pl\?index=(?P<bioguide>.*?)">(?P<lastname>.*?), (?P<firstname>.*?)</a></td><td>.*?</td>'
+    re_result_secondline = r'<td>.*?</td><td>(?P<party>.*?)</td><td align="center">(?P<state>\w\w)</td><td align="center">\d+<br>\((?P<startyear>\d{4})\-(?P<endyear>\d{4})\)</td></tr>'
+    results = []
+    for i in xrange(len(html_lines)):
+        line = html_lines[i]
+        matching_row = re.search(re_result_firstline, line.lower())
+        if matching_row:
+            data = {
+                'bioguide': matching_row.group('bioguide'),
+                'firstname': matching_row.group('firstname'),
+                'lastname': matching_row.group('lastname'),
+            }
+            i+=1
+            row_continued = re.search(re_result_secondline, html_lines[i].lower())
+            if row_continued:
+                data['party'] = row_continued.group('party')
+                data['state'] = row_continued.group('state')
+                data['years'] = (row_continued.group('startyear'), row_continued.group('endyear'))
+            results.append(data)
+    if results:
+        return results
+    else: return None
 
 
