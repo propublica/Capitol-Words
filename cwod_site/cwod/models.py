b9b095a2a66f79e36bbad1affaeb57b38e20803b
Aaron Bycoffe <abycoffe@sunlightfoundation.com>
2011-02-18 11:00:21 -0500

    Create model for storing total n-gram counts by date

--- cwod_site/cwod/models.py
+++ cwod_site/cwod/models.py
@@ -6,3 +6,16 @@ class CongressionalRecordVolume(models.Model):
     congress = models.IntegerField(db_index=True)
     session = models.CharField(max_length=10, db_index=True)
     volume = models.IntegerField()
+
+
+class NgramDateCount(models.Model):
+    """Storing the total number of ngrams per date
+    allows us to show the percentage of a given ngram
+    on a given date, mainly for graphing purposes.
+    """
+    n = models.IntegerField(db_index=True)
+    date = models.DateField(db_index=True)
+    count = models.IntegerField()
+
+    class Meta:
+        unique_together = (('n', 'date', ), )
