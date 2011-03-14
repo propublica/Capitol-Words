52b4edc07e2a69f14ba8629780761f839ca36f7b
Aaron Bycoffe <abycoffe@sunlightfoundation.com>
2011-03-14 15:20:26 -0400

    Update urls to reflect front-end structure

--- cwod_site/urls.py
+++ cwod_site/urls.py
@@ -3,12 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 
-        url(r'^$',
-            'django.views.generic.simple.direct_to_template',
-            {'template': 'index.html',
-            },
-            name='cwod_docs'),
-
         (r'^api/', include('cwod_api.urls')),
-        (r'^/', include('cwod.urls')),
+
+        (r'^', include('cwod.urls')),
 )
