13688287d64d289f9c20843a1821b451f1be5594
Aaron Bycoffe <abycoffe@sunlightfoundation.com>
2011-08-31 11:20:17 -0400

    Update urls

--- cwod_site/urls.py
+++ cwod_site/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
 
         (r'^api/', include('cwod_api.urls')),
 
+        (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
+        (r'^admin/', include(admin.site.urls)),
+
         (r'^', include('cwod.urls')),
 )
+
