5ce5e73d7e61c5bcf235a8ab05c196fb64ab4d78
Dan Drinkard <dan.drinkard@gmail.com>
2011-10-20 11:34:39 -0400

    'request' isn't an HTTPRequest instance in this case...

--- cwod_site/cwod_api/views.py
+++ cwod_site/cwod_api/views.py
@@ -13,7 +13,6 @@ from django.core.cache import cache
 from django.db.models import *
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django.db import connections, DatabaseError
-from django.template import RequestContext
 
 from bioguide.models import *
 from cwod_api.models import *
@@ -792,7 +791,7 @@ class FullTextSearchHandler(GenericHandler):
                     'speaking': x.get('speaking'),
                     'title': x.get('document_title', ''),
                     'origin_url': create_gpo_url(x.get('crdoc', '')),
-                    'capitolwords_url': EntryDetailUrlNode(x.get('document_title', ''), create_gpo_url(x.get('crdoc', ''))).render(RequestContext(data)),
+                    'capitolwords_url': EntryDetailUrlNode(x.get('document_title', ''), create_gpo_url(x.get('crdoc', ''))).render(data),
                     'speaker_first': x.get('speaker_firstname'),
                     'speaker_last': x.get('speaker_lastname'),
                     'speaker_party': x.get('speaker_party'),
