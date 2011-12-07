0224ebf5a801dee55f7f76bb113309628bb03727
Dan Drinkard <dan.drinkard@gmail.com>
2011-12-07 16:38:25 -0500

    updates to distance calc for further testing

--- cwod_site/ngrams/management/commands/calculate_distance.py
+++ cwod_site/ngrams/management/commands/calculate_distance.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 from math import sqrt
 from optparse import make_option
+from scipy.spatial.distance import cosine as cosine_distance
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connections, transaction
@@ -12,35 +13,35 @@ class Calculator(object):
         self.keypair = keypair
         self.ngram_map = {}
         for model in models:
-            key = model.__dict__[field]
+            key = model.__dict__[field if not field == 'bioguide' else 'bioguide_id']
             try:
                 self.ngram_map[model.ngram][key] = model.tfidf
             except KeyError:
                 self.ngram_map[model.ngram] = {key: model.tfidf}
 
     def calculate(self):
-        a = get_vector(self.keypair[0])
-        b = get_vector(self.keypair[1])
+        a = self.get_vector(self.keypair[0])
+        b = self.get_vector(self.keypair[1])
         return cosine_distance(a, b)
 
 
-    def cosine_distance(self, a, b):
-        '''Calculates the distance between n-dimensional vectors a, b
-           http://stackoverflow.com/questions/1823293/optimized-method-for-calculating-cosine-distance-in-python
-           '''
-        if len(a) != len(b):
-            raise ValueError, "a and b must be the same length"
-        numerator = 0
-        denoma = 0
-        denomb = 0
-        for i in range(len(a)):
-            ai = a[i]
-            bi = b[i]
-            numerator += ai*bi
-            denoma += ai*ai
-            denomb += bi*bi
-        result = 1 - numerator / (sqrt(denoma)*sqrt(denomb))
-        return result
+    # def cosine_distance(self, a, b):
+    #     '''Calculates the distance between n-dimensional vectors a, b
+    #        http://stackoverflow.com/questions/1823293/optimized-method-for-calculating-cosine-distance-in-python
+    #        '''
+    #     if len(a) != len(b):
+    #         raise ValueError, "a and b must be the same length"
+    #     numerator = 0
+    #     denoma = 0
+    #     denomb = 0
+    #     for i in range(len(a)):
+    #         ai = a[i]
+    #         bi = b[i]
+    #         numerator += ai*bi
+    #         denoma += ai*ai
+    #         denomb += bi*bi
+    #     result = 1 - numerator / (sqrt(denoma)*sqrt(denomb))
+    #     return result
 
     def get_vector(self, key):
         return tuple([ngram.get(key, 0) for ngram in self.ngram_map.values()])
@@ -56,7 +57,7 @@ class Command(BaseCommand):
             make_option('--values',
                 action='store',
                 dest='values',
-                default=None,
+                default='',
                 help='Specific values to limit calculations among'),
     )
 
@@ -71,21 +72,24 @@ class Command(BaseCommand):
         field = options.get('field')
         if not field:
             raise Exception('You must specify a field! Options are: date, month, state, bioguide')
-        values_to_compare = [value.strip() for value in kwargs.get(values).split(',')]
+        values_to_compare = [value.strip() for value in options.get('values').split(',')]
         cursor = connections['ngrams'].cursor()
-        query = 'SELECT DISTINCT %s from ngrams_ngramsby%s' % (field, field)
+        query = 'SELECT DISTINCT %s from ngrams_ngramsby%s' % (field if not field == 'bioguide' else 'bioguide_id',
+                                                               field)
         if values_to_compare:
             keys = set(values_to_compare)
         else:
-            keys = set([str(key) for key in cursor.execute('SELECT DISTINCT %s from ngrams_ngramsby%s').fetchall()])
+            cursor.execute(query)
+            keys = set([str(key[0]) for key in cursor.fetchall()])
         pairs = combinations(keys, 2)
 
         for keypair in pairs:
             # with transaction.commit_on_success():
             current_models = self._get_models(self.field, keypair)
-            calculator = Calculator(current_models, keypair)
+            calculator = Calculator(field, current_models, keypair)
             distance = calculator.calculate()
             # INSERT STUFF!
+            print "%s to %s: %s" % (keypair[0], keypair[1], distance)
 
     def _get_models(self, field, values_to_compare):
         if values_to_compare:
