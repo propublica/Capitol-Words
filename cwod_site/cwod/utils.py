daba6220735218883aa8a9eafb473f09cf3910e9
Dan Drinkard <dan.drinkard@gmail.com>
2012-03-08 17:17:05 -0500

    allow -txt- instead of pt1 when extracting kwargs from origin url

--- cwod_site/cwod/utils.py
+++ cwod_site/cwod/utils.py
@@ -5,7 +5,7 @@ from django.template.defaultfilters import slugify
 
 def get_entry_detail_url(origin_url, title):
     crec = origin_url.split('/')[-1].replace('.htm', '')
-    m = re.search(r'CREC-(?P<year>\d{4})-(?P<month>\d\d)-(?P<day>\d\d)-pt\d-Pg(?P<page_id>.*?)$', crec)
+    m = re.search(r'CREC-(?P<year>\d{4})-(?P<month>\d\d)-(?P<day>\d\d)-(pt\d|txt)-Pg(?P<page_id>.*?)$', crec)
     if not m:
         return ''
     kwargs = m.groupdict()
