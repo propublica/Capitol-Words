4ffdee7b9630576472cc139965ad8f201b2b4431
Dan Drinkard <dan.drinkard@gmail.com>
2013-11-21 16:11:37 -0500

    Sane ordering for list filters

--- cwod_site/bioguide/admin.py
+++ cwod_site/bioguide/admin.py
@@ -4,13 +4,13 @@ from bioguide.models import Legislator, LegislatorRole, ChangedBioguide
 
 class LegislatorAdmin(admin.ModelAdmin):
     list_display = ('bioguide_id', '__unicode__', 'birth_death', 'position', 'party', 'state', 'congress',)
-    list_filter = ('bioguide_id', 'position', 'party', 'state', 'congress',)
+    list_filter = ('congress', 'position', 'party', 'state','bioguide_id',)
     search_fields = ('bioguide_id', 'first', 'last', 'position', 'party', 'state', 'congress',)
 
 
 class LegislatorRoleAdmin(admin.ModelAdmin):
     list_display = ('bioguide_id', '__unicode__', 'congress', 'chamber', 'party', 'title', 'state', 'district', 'begin_date', 'end_date',)
-    list_filter = ('bioguide_id', 'congress', 'chamber', 'party', 'title', 'state', 'district', 'begin_date', 'end_date',)
+    list_filter = ('congress', 'chamber', 'party', 'title', 'state', 'district', 'begin_date', 'end_date', 'bioguide_id',)
     date_hierarchy = 'begin_date'
     search_fields = ('bioguide_id', 'first', 'last', 'congress', 'chamber', 'party', 'title', 'state', 'district',)
 
