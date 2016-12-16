688cad3a17c9a9e2e175e0e6560fc102c8a041ba
Christian Sieber <kazanir@gmail.com>
2016-12-16 14:54:49 -0700

    Refactor daily scripts
    - Assume the existence of proper shell environment variables for the
      application root, log folder, and Python virtual environment.
    - Move crontab file to correctly-named file and update for new env
      variables.
    - Fix all execution scripts to use new environment variables rather
      than hard-coding all remote URLs.

--- daily_update.sh
+++ daily_update.sh
@@ -7,38 +7,38 @@ date_count_date=`date -d '1 day ago' +'%Y-%m-%d'`
 scraper_yesterday=`date -d '1 day ago' +'%d/%m/%Y'`
 
 # Scrape yesterday's record
-/projects/capwords/bin/python /projects/capwords/src/Capitol-Words/scraper/scraper.py backto $scraper_yesterday
+$CAPWORDS_VENV/bin/python $CAPWORDS_HOME/scraper/scraper.py backto $scraper_yesterday
 
-source /projects/capwords/bin/activate
+source $CAPWORDS_ENV/bin/activate
 
 # Ingest if congress was in session
 if [ -d /opt/data/raw/$yesterday ]; then
 
   for i in `find /opt/data/raw/$yesterday -name '*PgH*.txt'`; do
-      /projects/capwords/bin/python /projects/capwords/src/Capitol-Words/parser/parser.py $i;
+      $CAPWORDS_ENV/bin/python $CAPWORDS_HOME/parser/parser.py $i;
   done
 
   for i in `find /opt/data/raw/$yesterday -name '*PgS*.txt'`; do
-      /projects/capwords/bin/python /projects/capwords/src/Capitol-Words/parser/parser.py $i;
+      $CAPWORDS_ENV/bin/python $CAPWORDS_HOME/parser/parser.py $i;
   done
 
   for i in `find /opt/data/raw/$yesterday -name '*PgE*.txt'`; do
-      /projects/capwords/bin/python /projects/capwords/src/Capitol-Words/parser/parser.py $i;
+      $CAPWORDS_ENV/bin/python $CAPWORDS_HOME/parser/parser.py $i;
   done
 
   for i in `find /opt/data/xml/$yesterday -mtime -1 -name '*.xml'`; do
-      /projects/capwords/bin/python /projects/capwords/src/Capitol-Words/solr/ingest.py $i --solrdocs-only;
+      $CAPWORDS_ENV/bin/python $CAPWORDS_HOME/solr/ingest.py $i --solrdocs-only;
   done
 
-  find /opt/data/solrdocs/$yesterday -mtime -1 -name '*.xml' -exec curl -d @{} http://ec2-184-72-184-231.compute-1.amazonaws.com:8983/solr/update -H "Content-Type: text/xml" \;
+  find /opt/data/solrdocs/$yesterday -mtime -1 -name '*.xml' -exec curl -d @{} $CAPWORDS_SOLR_URL/update -H "Content-Type: text/xml" \;
 
-  curl "http://ec2-184-72-184-231.compute-1.amazonaws.com:8983/solr/update?commit=true"
+  curl "$CAPWORDS_SOLR_URL/update?commit=true"
 
-  /usr/bin/env python /projects/capwords/src/Capitol-Words/cwod_site/manage.py get_date_counts --date=$date_count_date
-  /usr/bin/env python /projects/capwords/src/Capitol-Words/cwod_site/manage.py calculate_ngram_tfidf --field=date
-  /usr/bin/env python /projects/capwords/src/Capitol-Words/cwod_site/manage.py cache_recent_entries
+  /usr/bin/env python $CAPWORDS_HOME/cwod_site/manage.py get_date_counts --date=$date_count_date
+  /usr/bin/env python $CAPWORDS_HOME/cwod_site/manage.py calculate_ngram_tfidf --field=date
+  /usr/bin/env python $CAPWORDS_HOME/cwod_site/manage.py cache_recent_entries
 
 fi
 
 # Always run these
-/usr/bin/env python /projects/capwords/src/Capitol-Words/cwod_site/manage.py apireportfromlogs
+/usr/bin/env python $CAPWORDS_HOME/cwod_site/manage.py apireportfromlogs
