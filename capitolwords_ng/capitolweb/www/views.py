191d934d13b9e168e5a60f6f812a370212bbb6b3
Rick Mangi <rick@chartbeat.com>
2017-05-26 09:47:54 -0400

    www app for, you know, www stuff

---
+++ capitolwords_ng/capitolweb/www/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("#resist")
