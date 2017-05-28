09c3fd65545085384bd6b415d3dd7cc38cb6e94b
will-horning <will.horning@gmail.com>
2017-05-28 19:22:43 -0400

    [parser] reverted parser to be a little less data type agnostic for expediency

--- capitolwords_ng/parsers/crec.py
+++ capitolwords_ng/parsers/crec.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 
 import logging
+from datetime import datetime
+
+import boto3
 from lxml import etree
 
 
@@ -23,6 +26,7 @@ CREC_BUCKET = 'capitol-words-data'
 
 
 CREC_PREFIX_TEMPLATE = 'crec/%Y/%m/%d/crec'
+CREC_KEY_TEMPLATE = '{prefix}/{id}.htm'
 
 
 def xpath_parse(root, paths, namespaces):
@@ -46,12 +50,14 @@ def xpath_parse(root, paths, namespaces):
 
 class CRECParser(object):
 
-    def __init__(self, xml_namespace=None, xpaths=DEFAULT_XPATHS):
+    def __init__(self, bucket='capitol-words-data', xml_namespace=None, xpaths=DEFAULT_XPATHS):
         if xml_namespace is None:
             self.xml_namespace = DEFAULT_XML_NS
         else:
             self.xml_namespace = xml_namespace
         self.xpaths = xpaths
+        self.s3 = boto3.client('s3')
+        self.bucket = bucket
 
     def parse_mods_file(self, mods_file):
         doc = etree.parse(mods_file)
@@ -59,12 +65,36 @@ class CRECParser(object):
             '//ns:relatedItem[@type="constituent"]',
             namespaces=DEFAULT_XML_NS,
         )
-        date_issued = doc.xpath(
-            'string(//ns:originInfo/ns:dateIssued)',
-            namespaces=DEFAULT_XML_NS,
-        )
         records = [
             xpath_parse(r, self.xpaths, namespaces=self.xml_namespace)
             for r in constituents
         ]
+        date_issued_str = doc.xpath(
+            'string(//ns:originInfo/ns:dateIssued)',
+            namespaces=DEFAULT_XML_NS,
+        )
+        date_issued = datetime.strptime(date_issued_str, '%Y-%m-%d')
+        prefix = date_issued.strftime(CREC_PREFIX_TEMPLATE)
+        for record in records:
+            record['date_issued'] = date_issued
+            s3_key = CREC_KEY_TEMPLATE.format(
+                prefix=prefix,
+                id=record['ID'].strip('id-')
+            )
+            failed_retrievals = []
+            try:
+                response = self.s3.get_object(Bucket=self.bucket, Key=s3_key)
+                record['content'] = response['Body'].read()
+            except Exception as e:
+                failed_retrievals.append(s3_key)
+        if len(failed_retrievals) > 0:
+            logging.error(
+                'Failed to rerieve {0}/{1} crec docs.'.format(
+                    len(failed_retrievals), len(records)
+                )
+            )
+            for s3_key in failed_retrievals:
+                logging.error('Failed retrieval: "{0}".'.format(s3_key))
+        else:
+            logging.info('All crec retrievals succeeded.')
         return records
---
+++ capitolwords_ng/run_crec_es_uploader.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+
+import os
+import json
+import logging
+import argparse
+import urlparse
+from datetime import datetime
+from datetime import timedelta
+
+import elasticsearch
+import boto3
+
+from cli import setup_logger
+from cli import add_logging_options
+from cli import CMD_LINE_DATE_FORMAT
+from parsers.crec import CRECParser
+
+
+MODS_KEY_TEMPLATE = 'crec/%Y/%m/%d/mods/mods.xml'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    add_logging_options(parser)
+    output_option_group = parser.add_mutually_exclusive_group(required=True)
+    output_option_group.add_argument(
+        '--to_stdout',
+        help='If true, will not upload to es and instead print to stdout.',
+        action='store_true'
+    )
+    output_option_group.add_argument(
+        '--es_url',
+        help='Elastic search URL to upload records to.',
+    )
+    parser.add_argument(
+        '--file_path',
+        help='Path to file to be parsed.'
+    )
+    parser.add_argument(
+        '--start_dt',
+        help='Start of date range to pull data for, inclusive.',
+        type=lambda d: datetime.strptime(d, CMD_LINE_DATE_FORMAT),
+        default=None,
+    )
+    parser.add_argument(
+        '--end_dt',
+        help='End of date range to pull data for, exclusive.',
+        type=lambda d: datetime.strptime(d, CMD_LINE_DATE_FORMAT),
+        default=None,
+    )
+    parser.add_argument(
+        '--source_bucket',
+        help='Location of crec data.',
+    )
+    args = parser.parse_args()
+
+    parser = CRECParser(
+        bucket=args.source_bucket,
+    )
+    records = []
+    if args.file_path:
+        input_stream = open(args.source_path)
+        records = parser.parse_mods_file(input_stream)
+    else:
+        s3 = boto3.client('s3')
+        dt = args.start_dt.replace(hour=0, minute=0, second=0, microsecond=0)
+        if args.es_url:
+            parsed_es_url = urlparse.urlparse(args.es_url)
+            es_host = parsed_es_url.netloc
+            index = parsed_es_url.path.strip('/')
+            es_conn = elasticsearch.Elasticsearch([es_host])
+        while dt < args.end_dt:
+            print(dt)
+            try:
+                response = s3.get_object(
+                    Bucket=args.source_bucket,
+                    Key=dt.strftime(MODS_KEY_TEMPLATE)
+                )
+            except Exception as e:
+                logging.info('Could not find mods file for {0}.'.format(dt))
+                response = None
+            if response is not None and response.get('Body'):
+                input_stream = response['Body']
+                records = parser.parse_mods_file(input_stream)
+                for r in records:
+                    es_conn.index(index=index, doc_type='crec', id=r['ID'], body=r)
+            dt += timedelta(days=1)
+
+    if args.to_stdout:
+        for r in records:
+            print(r)
+    else:
+        parsed_es_url = urlparse.urlparse(args.es_url)
+        es_host = parsed_es_url.netloc
+        index = parsed_es_url.path.strip('/')
+        es_conn = elasticsearch.Elasticsearch([es_host])
+        for r in records:
+            es_conn.index(index=index, doc_type='crec', id=r['ID'], body=r)
+    logging.info('Extracted {0} records.'.format(len(records)))
