b7ffe130ad03900c9125aa1d3670d1a7023a2905
Nick Moy <nick@chartbeat.com>
2017-05-31 09:38:59 -0400

    Load index page from local webpack dev server to run the frontend SPA

--- capitolwords_ng/capitolweb/capitolweb/settings.py
+++ capitolwords_ng/capitolweb/capitolweb/settings.py
@@ -17,6 +17,9 @@ import os
 ES_URL = 'es://capitolwords.chartbeat.net:80'
 ES_CW_INDEX = 'will-test'
 
+DEV_FRONTEND = True
+DEV_FRONTEND_SPA_BASE_URL = 'http://localhost:3000'
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
