070319bf0852015525ab2f7e72a081297d803ff3
will-horning <will.horning@gmail.com>
2017-05-31 17:35:55 -0400

    [es_uploader] stripped out file path testing stuff in es uploader

--- capitolwords_ng/run_crec_es_uploader.py
+++ capitolwords_ng/run_crec_es_uploader.py
@@ -34,10 +34,6 @@ if __name__ == '__main__':
         help='Elastic search URL to upload records to.',
     )
     parser.add_argument(
-        '--file_path',
-        help='Path to file to be parsed.'
-    )
-    parser.add_argument(
         '--start_dt',
         help='Start of date range to pull data for, inclusive.',
         type=lambda d: datetime.strptime(d, CMD_LINE_DATE_FORMAT),
@@ -58,41 +54,34 @@ if __name__ == '__main__':
     parser = CRECParser(
         bucket=args.source_bucket,
     )
-    if args.file_path:
-        input_stream = open(args.source_path)
-        records = parser.parse_mods_file(input_stream)
-        print('Using stdout:')
-        for r in records:
-            print(r)
-    else:
-        s3 = boto3.client('s3')
-        dt = args.start_dt.replace(hour=0, minute=0, second=0, microsecond=0)
-        if args.es_url:
-            parsed_es_url = urlparse.urlparse(args.es_url)
-            es_host = parsed_es_url.netloc
-            index = parsed_es_url.path.strip('/')
-            es_conn = elasticsearch.Elasticsearch([es_host])
-        while dt < args.end_dt:
-            print('Processing files for {0}.'.format(dt))
-            try:
-                response = s3.get_object(
-                    Bucket=args.source_bucket,
-                    Key=dt.strftime(MODS_KEY_TEMPLATE)
-                )
-            except Exception as e:
-                print('Could not find mods file for {0}.'.format(dt))
-                response = None
-            if response is not None and response.get('Body'):
-                input_stream = response['Body']
-                new_records = parser.parse_mods_file(input_stream)
-                print('Found {0} new records.'.format(len(new_records)))
-                if args.to_stdout:
-                    print('Using stdout:')
-                    for r in new_records:
-                        print(r)
-                else:
-                    for r in new_records:
-                        es_conn.index(
-                            index=index, doc_type='crec', id=r['ID'], body=r
-                        )
-            dt += timedelta(days=1)
+    s3 = boto3.client('s3')
+    dt = args.start_dt.replace(hour=0, minute=0, second=0, microsecond=0)
+    if args.es_url:
+        parsed_es_url = urlparse.urlparse(args.es_url)
+        es_host = parsed_es_url.netloc
+        index = parsed_es_url.path.strip('/')
+        es_conn = elasticsearch.Elasticsearch([es_host])
+    while dt < args.end_dt:
+        print('Processing files for {0}.'.format(dt))
+        try:
+            response = s3.get_object(
+                Bucket=args.source_bucket,
+                Key=dt.strftime(MODS_KEY_TEMPLATE)
+            )
+        except Exception as e:
+            print('Could not find mods file for {0}.'.format(dt))
+            response = None
+        if response is not None and response.get('Body'):
+            input_stream = response['Body']
+            new_records = parser.parse_mods_file(input_stream)
+            print('Found {0} new records.'.format(len(new_records)))
+            if args.to_stdout:
+                print('Using stdout:')
+                for r in new_records:
+                    print(r)
+            else:
+                for r in new_records:
+                    es_conn.index(
+                        index=index, doc_type='crec', id=r['ID'], body=r
+                    )
+        dt += timedelta(days=1)
