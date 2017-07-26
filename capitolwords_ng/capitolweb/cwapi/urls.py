e468579c2aa73f4994adeef7b8348ed43ad002e3
Albert Toledo <albert@chartbeat.com>
2017-07-26 14:40:31 -0400

    [cwapi] add new api endpoint that returns count of a term for given time period

--- capitolwords_ng/capitolweb/cwapi/urls.py
+++ capitolwords_ng/capitolweb/cwapi/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^search/title/(?P<title>.*)/$', views.search_by_title, name='search_by_title'),
     url(r'^search/entities/$', views.search_by_entities, name='search_by_entities'),
     url(r'^search/multi/$', views.search_by_params, name='search_by_params'),
+    url(r'^count/(?P<term>\w+)/$', views.count_of_term_in_content, name='count_of_term_in_content'),
 
 ]
--- capitolwords_ng/capitolweb/cwapi/views.py
+++ capitolwords_ng/capitolweb/cwapi/views.py
@@ -6,6 +6,7 @@ from elasticsearch_dsl import Search
 from elasticsearch_dsl.query import Match, Q, Range
 from elasticsearch_dsl.connections import connections
 from rest_framework.decorators import api_view
+import datetime
 
 import logging
 
@@ -19,6 +20,12 @@ QUERIES = {
     'speaker': 'get_speaker',
     'content': 'get_content',
     'entity': 'get_entity'
+}
+
+TIME_PERIODS = {
+    'day': 1,
+    'week': 7,
+    'month': 30,
 }
 
 
@@ -64,7 +71,7 @@ def get_date_range(start, end="now/d"):
 def search_by_speaker(request, name):
     """
     Search for a speaker by name
-    :param request: 
+    :param request:
     :param name: name of the congress person speaking
     :return: list sorted by date_issued
     """
@@ -73,12 +80,11 @@ def search_by_speaker(request, name):
     if response.success():
         return JsonResponse(response.to_dict())
 
-
 @api_view(['GET'])
 def search_by_title(request, title):
     """
     Search by title of a document
-    :param request: 
+    :param request:
     :param title: the title
     :return: list of results sorted by date_issued
     """
@@ -104,26 +110,25 @@ def search_by_entities(request):
     if response.success():
         return JsonResponse(response.to_dict())
 
-
 @api_view(['GET'])
 def search_by_params(request):
     """
     Search by arbitrary params which can be combined and can also be lists
-    
+
     title - search on the title field
     speaker - individual speakers
     content - search for text matches in the content add &highlight=<number> to include contextual
               matches of the given fragment size in the result (default is 200)
     entity - named entities
     start_date / end_date in form 2017-01-04, inclusive. end_date defaults to today.
-    
+
     Example:
         http://localhost:8000/cwapi/search/multi/?title=Budget&entity=social%20security&entity=senate&speaker=sanders
         http://localhost:8000/cwapi/search/multi/?speaker=schumer&content=trump&highlight=1000
         http://localhost:8000/cwapi/search/multi/?title=Budget&speaker=sanders&start_date=2016-01-11&end_date=2017-01-04
 
-    :param request: 
-    :return: 
+    :param request:
+    :return:
     """
     logger.debug("search_by_params")
     params = request.GET
@@ -152,3 +157,48 @@ def search_by_params(request):
         return JsonResponse(response.to_dict())
     return JsonResponse("Found nothing")
 
+@api_view(['GET'])
+def count_of_term_in_content(request, term):
+    """
+    Count the mention of a term in content
+
+    time_period(optional, default to 'month') - period of time to go
+                                                back from today
+                                                month | week | day
+
+    Example:
+        http://127.0.0.1:8000/cwapi/count/obamacare/?time_period=week
+        http://127.0.0.1:8000/cwapi/count/obamacare/
+
+    :param request:
+    :param term: term whose occurances we are counting
+    :return: number of occurances of the term in content
+    """
+    time_period = 'month'
+    if 'time_period' in request.query_params:
+        time_period = request.query_params.get('time_period')
+
+    query = get_content(term)
+    search = make_search()
+
+    # Make this 0 or just remove when we have more recent data
+    starting_delta = -365
+
+    delta = TIME_PERIODS[time_period]
+    today = datetime.datetime.today()
+    start_date = today + datetime.timedelta(starting_delta - delta)
+    end_date = today + datetime.timedelta(starting_delta)
+
+    # gte: Greater or Equal, lte: Better than 4G? JK Less than or Equal
+    date_filter = {'range':{'date_issued':{'gte': start_date, 'lte':end_date}}}
+    count = search.query(query).filter(date_filter).count()
+
+    return JsonResponse(
+        {
+            'term': term,
+            'count': count,
+            'time_period': time_period,
+            'start_date': start_date,
+            'end_date': end_date,
+        }
+    )
