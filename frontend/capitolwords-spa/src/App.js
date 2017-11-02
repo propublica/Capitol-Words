81babab39070a144baa38dc21dfa603053850bf2
Albert Toledo <albert@chartbeat.com>
2017-11-02 16:22:53 -0400

    [fe] added homepage

--- frontend/capitolwords-spa/src/App.js
+++ frontend/capitolwords-spa/src/App.js
@@ -5,20 +5,49 @@ import './App.css';
 
 import { phraseSearchRequested } from './actions/search-actions';
 
+import { searchContent, isSearching } from './selectors/phrase-search-selectors';
+
 import HeaderBar from './components/HeaderBar/HeaderBar';
 import PhraseSearchResults from './components/PhraseSearchResults/PhraseSearchResults';
+import HomePage from './components/HomePage/HomePage';
+
+import { ClipLoader } from 'react-spinners';
 
 class App extends Component {
+
+  renderMainContent() {
+    const { searchContent, isSearching } = this.props;
+    const mainContent = searchContent ? <PhraseSearchResults /> : <HomePage />;
+    const content = isSearching ? this.renderLoader() : mainContent;
+    return (
+      <div className="Main-container">
+        {content}
+      </div>
+    );
+  }
+
+  renderLoader() {
+    return (
+      <div className="Loader-container">
+        <ClipLoader color="#9CAB4C"/>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App-container">
         <HeaderBar onSearchSubmit={this.props.phraseSearchRequested}/>
-        <PhraseSearchResults />
+        {this.renderMainContent()}
       </div>
     );
   }
 }
 
-export default connect(
-  null,
-  {phraseSearchRequested})(App);
+export default connect(state => ({
+    searchContent: searchContent(state),
+    isSearching: isSearching(state),
+  }),
+  {
+    phraseSearchRequested,
+  })(App);
---
+++ frontend/capitolwords-spa/src/components/HomePage/HomePage.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Highlighter from 'react-highlight-words'
+
+import { phraseSearchRequested } from '../../actions/search-actions';
+
+import SearchInput from '../SearchInput/SearchInput';
+
+import './HomePage.css';
+
+import {
+  isSearching,
+  isSearchFailure,
+  isSearchSuccess,
+  searchContent,
+  searchResultList,
+  searchDelta
+} from '../../selectors/phrase-search-selectors';
+
+class HomePage extends Component {
+  static propTypes = {
+    isSearching: PropTypes.bool.isRequired,
+    isSearchFailure: PropTypes.bool.isRequired,
+    isSearchSuccess: PropTypes.bool.isRequired,
+    searchResultList: PropTypes.array,
+    searchContent: PropTypes.string,
+    searchDelta: PropTypes.number
+  };
+
+
+  render() {
+    return (
+      <div className="HomePage-container">
+        <div className="Title">
+          <h3>Capitol Words</h3>
+          <p>Dig up some data on the words our legislators use every day.</p>
+        </div>
+        <div className="SearchInput-container">
+          <SearchInput onSubmit={this.props.phraseSearchRequested}/>
+        </div>
+        <div className="QuickOptions-container">
+          <div className="QuickOption" onClick={() => {this.props.phraseSearchRequested('Poverty')}}>Poverty</div>
+          <div className="QuickOption" onClick={() => {this.props.phraseSearchRequested('Russian Interference')}}>Russian Interference</div>
+          <div className="QuickOption" onClick={() => {this.props.phraseSearchRequested('Education')}}>Education</div>
+          <div className="QuickOption" onClick={() => {this.props.phraseSearchRequested('Tax Cuts')}}>Tax Cuts</div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+export default connect(state => ({
+  isSearching: isSearching(state),
+  isSearchFailure: isSearchFailure(state),
+  isSearchSuccess: isSearchSuccess(state),
+  searchContent: searchContent(state),
+  searchDelta: searchDelta(state),
+  searchResultList: searchResultList(state),
+}),
+{
+  phraseSearchRequested,
+})(HomePage);
