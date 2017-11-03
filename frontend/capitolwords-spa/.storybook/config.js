1f3bde862483dd48e881b58d9a01e480765d6c43
Nick Moy <nick@chartbeat.com>
2017-11-03 17:56:02 -0400

    [frontend] Fixup storybook setup and add glamorous packages

--- frontend/capitolwords-spa/.storybook/config.js
+++ frontend/capitolwords-spa/.storybook/config.js
@@ -1,7 +1,28 @@
-import { configure } from '@kadira/storybook';
+import React from 'react';
+import g from 'glamorous';
+
+import {
+  configure,
+  addDecorator
+} from '@storybook/react';
+
+import '../src/index.css';
+
+
+// All stories should be in the same directory in which the target of the
+// stories (the React Component) is defined. The story filenames are expected to
+// be in the format `<name>.story.js`.
+const req = require.context('../src', true, /\.story\.js$/);
+
+// Add global stories decorator.
+addDecorator((story) => (
+  <g.Div padding="48px">
+    {story()}
+  </g.Div>
+));
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(req)
 }
 
 configure(loadStories, module);
